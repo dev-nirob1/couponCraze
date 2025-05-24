06e75575ec508b406812100d2e9fb3569e44c852
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-25 00:18:32 +0600

    update modal responsive for mobile device

--- vendor/index.js
+++ vendor/index.js
@@ -20,8 +20,34 @@ const navbarAnimation = () => {
   });
 };
 
-const getCode = () => {
-  const getBtn = document.getElementById('get-code');
-}
-navbarAnimation()
+const getCodeButton = document.querySelectorAll(".coupon-card button");
+const closeModal = document.querySelectorAll(".modal close-btn");
+const modal = document.querySelector('.modal')
 
+getCodeButton.forEach(open => {
+  open.addEventListener("click", () => {
+    modal.style.display = 'flex'
+  });
+})
+
+closeModal.forEach(close => {
+  close.addEventListener("click", () => {
+    modal.style.display = 'none'
+    console.log("close modal");
+  });
+  
+})
+
+// console.log(getCodeButton);
+// const getCode = () => {
+//   getBtn.addEventListener("click", () => {
+//     isModalOpen = true;
+//   });
+//   closeModal.addEventListener("click", () => {
+//     isModalOpen = false;
+//     console.log(isModalOpen);
+//   });
+// };
+// getCode();
+
+navbarAnimation();
