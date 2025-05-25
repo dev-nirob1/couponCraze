d9e64483e8ffab9eb094f7d9a6b44365358344cc
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-25 23:58:09 +0600

    slider added

---
+++ vendor/slider.js
@@ -0,0 +1,35 @@
+const slider = () => {
+  const slideContainer = document.querySelector(".slider-container");
+  const nextBtn = document.querySelector("#next-btn");
+  const prevBtn = document.querySelector("#prev-btn");
+  const sliders = document.querySelectorAll(".slider");
+  console.log("sliders", sliders);
+
+  let currentSlide = 0;
+
+  const showSlide = (i) => {
+    currentSlide = (i + sliders.length) % sliders.length;
+
+    sliders.forEach((el) => {
+      el.style.display = "none";
+    });
+
+    sliders[currentSlide].style.display = "block";
+  };
+
+  showSlide(currentSlide);
+
+  nextBtn.addEventListener("click", () => {
+    showSlide(currentSlide + 1);
+    // console.log("clicked next btn");
+  });
+  prevBtn.addEventListener("click", () => {
+    showSlide(currentSlide - 1);
+    //   console.log('clicked prev btn')
+  });
+  
+  let slideInterval = setInterval(() => {
+  showSlide(currentSlide + 1);
+}, 3000);
+};
+slider();
