1f329b3e06d7a00d25abcd2cc8a0d861123b5fdd
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-27 20:28:37 +0600

    update

--- vendor/index.js
+++ vendor/index.js
@@ -98,4 +98,4 @@ const owlCarousel = () => {
   });
 };
 navbarAnimation();
-// owlCarousel();
+owlCarousel();
--- vendor/swiper.js
+++ vendor/swiper.js
@@ -1,7 +1,1 @@
-const swiper = new Swiper(".swiper", {
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+
