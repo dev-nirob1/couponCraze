d7add672d0ee494a59fb34c7eff5feab310b6674
dev-nirob1 <dev.nirob1@gmail.com>
2025-05-27 20:38:00 +0600

    update swiper js

--- vendor/swiper.js
+++ vendor/swiper.js
@@ -1,1 +1,14 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  cssMode: true,
+  autoPlay: {
+    duration: 3000
+  },
+  navigation: {
+    nextEl: ".button-prev",
+    prevEl: ".button-next",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
 
